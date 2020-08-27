9bb04042633da0b1ceb662a869f30cd37f8cb357
Nozomu Kaneko <nozom.kaneko@gmail.com>
2020-08-28 04:19:37 +0900

    garden

--- frontend_site/frontend_site/custom_dbtemplates/models.py
+++ frontend_site/frontend_site/custom_dbtemplates/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.sites.managers import CurrentSiteManager
 from django.utils.translation import ugettext_lazy as _
 from django.utils.timezone import now
 
