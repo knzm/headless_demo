09466247d35c288bb7ebcf1f420e5c189e229577
Nozomu Kaneko <nozom.kaneko@gmail.com>
2020-09-06 17:20:39 +0900

    frontend: redirect with slash

--- frontend_site/frontend_site/routes/views.py
+++ frontend_site/frontend_site/routes/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.http import Http404
+from django.http import Http404, HttpResponsePermanentRedirect
 from django.template.response import TemplateResponse
+from django.utils.http import escape_leading_slashes
 
 import requests
 
@@ -10,6 +11,12 @@ from .models import find_route
 def page_view(request, path):
     m = find_route(path)
     if m is None:
+        if settings.APPEND_SLASH and not path.endswith('/'):
+            m = find_route(f'{path}/')
+            if m is not None:
+                new_path = request.get_full_path(force_append_slash=True)
+                new_path = escape_leading_slashes(new_path)
+                return HttpResponsePermanentRedirect(new_path)
         raise Http404
 
     params = {
